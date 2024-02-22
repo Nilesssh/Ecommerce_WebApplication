ba6594fa92ca6f4719c9f9f5cf7364295d138971
Nilesssh <nilesh30111997@gmail.com>
2024-02-22 15:47:01 +0530

    navbar ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-
+import Navbar from "./components/Navbar";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar/>
       <h1 className="text-white bg-slate-900 text-3xl">
         Welcome to the E-commerce webApplication
       </h1>
